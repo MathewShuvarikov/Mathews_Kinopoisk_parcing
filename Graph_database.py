4a1ad9dc3b29e2194a7a1122a66a42d2b2de03e3
MathewShuvarikov <shuvarikoff20@gmail.com>
2024-01-05 19:24:00 +0300

    Update

--- Graph_database.py
+++ Graph_database.py
@@ -3,24 +3,28 @@ import pandas as pd
 from kinopoisk_unofficial.kinopoisk_api_client import KinopoiskApiClient # connection with API
 from kinopoisk_unofficial.request.films.film_request import FilmRequest # film name
 from kinopoisk_unofficial.request.staff.staff_request import StaffRequest # People from film
+from kinopoisk_unofficial.client.exception.not_found import NotFound
 
 # empty dataframe to keep all the records
 data = pd.DataFrame(columns=['Film', 'Actor', 'Director', 'Screenwriter'])
 
-for i in range(350, 800):
+for i in range(700, 900):
+    try:
     ## 62b2be73-aae3-4751-a556-742d02d31d96 2nd api
     ## c6d5218b-d822-4f5f-850f-4b74b69d0499 1st api
-    api_client = KinopoiskApiClient("62b2be73-aae3-4751-a556-742d02d31d96") # my tocken
-    film_request = FilmRequest(i) # create a query for the film name using film id
-    staff_request = StaffRequest(i) # create a query for the film staff using film id
-    film_response = api_client.films.send_film_request(film_request)
-    staff_response = api_client.staff.send_staff_request(staff_request)
+        api_client = KinopoiskApiClient("c6d5218b-d822-4f5f-850f-4b74b69d0499") # my tocken
+        film_request = FilmRequest(i) # create a query for the film name using film id
+        staff_request = StaffRequest(i) # create a query for the film staff using film id
+        film_response = api_client.films.send_film_request(film_request)
+        staff_response = api_client.staff.send_staff_request(staff_request)
 
-    staff_by_profession = {}
-    for staff in staff_response.items:
-        profession = staff.profession_text
-        if profession not in staff_by_profession:
-            staff_by_profession[profession] = staff.name_ru
+        staff_by_profession = {}
+        for staff in staff_response.items:
+            profession = staff.profession_text
+            if profession not in staff_by_profession:
+                staff_by_profession[profession] = staff.name_ru
+    except NotFound:
+        print(f"Film with ID {i} not found. Skipping...")
 
     # Create a dictionary to hold data for the DataFrame
     data_dict = {'Film': film_response.film.name_ru}
