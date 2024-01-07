dec9bb44c2f3c42d62e0471c309970e6c81af2aa
MathewShuvarikov <shuvarikoff20@gmail.com>
2024-01-07 17:07:25 +0300

    Update

--- Graph_database_full.py
+++ Graph_database_full.py
@@ -2,7 +2,6 @@
 # Версия Pandas: 2.1.4
 # Версия neo4j: 5.16.0
 # Версия Python: 3.9.0 (tags/v3.9.0:9cf6752, Oct  5 2020, 15:34:40) [MSC v.1927 64 bit (AMD64)]
-import pandas as pd
 from kinopoisk_unofficial.kinopoisk_api_client import KinopoiskApiClient # connection with API
 from kinopoisk_unofficial.request.films.film_request import FilmRequest # film name
 from kinopoisk_unofficial.request.staff.staff_request import StaffRequest # People from film
@@ -13,8 +12,8 @@ relationships = []
 
 for i in range(300, 500):
     try:
-    ## 62b2be73-aae3-4751-a556-742d02d31d96 2nd api
-    ## c6d5218b-d822-4f5f-850f-4b74b69d0499 1st api
+    ## 62b2be73-aae3-4751-a556-742d02d31d96 # my 2nd api
+    ## c6d5218b-d822-4f5f-850f-4b74b69d0499 # my 1st api
         api_client = KinopoiskApiClient("c6d5218b-d822-4f5f-850f-4b74b69d0499") # мой токен
         film_request = FilmRequest(i) # запрос на название фильма с использованием film id
         staff_request = StaffRequest(i) # запрос на персон фильма с использованием film id
@@ -43,17 +42,17 @@ for i in range(300, 500):
 
 
 from neo4j import GraphDatabase
-# Connect to the Neo4j database
+# Задаем параметры для подключения к Neo4j
 # 9bf936d8.databases.neo4j.io:7687
 uri = "neo4j+s://9bf936d8.databases.neo4j.io:7687"  # server URI
 user = "neo4j"  # Neo4j username
 password = "wIeMFbVDn0n4lJRnABhWIv9NmUa7sE4WuJGXPk9pAHk"  # Neo4j password
 
-# Create a Neo4j driver instance
+# Подключаемся к Neo4j
 driver = GraphDatabase.driver(uri, auth=(user, password))
 
 def create_relationships(tx, film, person, relationship):
-    # Cypher query to create relationships between films and people
+    # Cypher-запрос на создание связей между людьми и фильмами
     query = (
         "MERGE (f:Film {title: $film}) "
         "MERGE (p:Person {name: $person}) "
@@ -62,7 +61,7 @@ def create_relationships(tx, film, person, relationship):
     )
     tx.run(query, film=film, person=person, relationship=relationship)
 
-# Iterate through relationships and import them to Neo4j
+# Проходим по связям и записываем их на сервер Neo4j
 with driver.session() as session:
     for relationship in relationships:
         film, person, rel_type = relationship
